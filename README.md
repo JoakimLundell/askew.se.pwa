d5d677637ab049f36f7d79540cdb45a40536b654
Joakim Lundell <joakim.lundell@evry.com>
2022-03-17 11:15:43 +0100

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# askew.se.pwa
-The PWA version of askew.se
+# askew.se.temp
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <div class="page-width">
+            <slot></slot>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    background: red;
+    border-bottom: 4px solid black;
+}
+.page-width {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 800px;
+}
+</style>
